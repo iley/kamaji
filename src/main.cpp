f6707ebdbb98b2252bd569e30985aaed022a8113
Ilya Strukov <iley@iley.ru>
2017-04-19 21:01:30 +0200

    add reset button

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,37 @@
 const int kButtonCount = 3;
 const int kButtonPins[] = {2, 3, 4};
 const int kLedPins[] = {17, 16, 15};
+const int kResetButtonPin = 6;
+
+bool gotWinner = false;
 
 void setup() {
   for (const int buttonPin : kButtonPins) {
     pinMode(buttonPin, INPUT);
   }
+  pinMode(kResetButtonPin, INPUT);
   for (const int ledPin : kLedPins) {
     pinMode(ledPin, OUTPUT);
   }
 }
 
 void loop() {
-  for (int i = 0; i < kButtonCount; ++i) {
-    int buttonState = digitalRead(kButtonPins[i]);
-    digitalWrite(kLedPins[i], buttonState);
+  if (gotWinner) {
+    int resetButtonState = digitalRead(kResetButtonPin);
+    if (resetButtonState == HIGH) {
+      gotWinner = false;
+      for (int ledPin : kLedPins) {
+        digitalWrite(ledPin, LOW);
+      }
+    }
+  } else {
+    for (int i = 0; i < kButtonCount; ++i) {
+      int buttonState = digitalRead(kButtonPins[i]);
+      if (buttonState == HIGH) {
+        digitalWrite(kLedPins[i], HIGH);
+        gotWinner = true;
+        break;
+      }
+    }
   }
 }
