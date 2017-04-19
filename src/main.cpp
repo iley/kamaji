512e2082aaf2676fcb1c0b0a50dcab3288ed2555
Ilya Strukov <iley@iley.ru>
2017-04-19 21:16:56 +0200

    add speaker

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "Arduino.h"
+#include "pitches.h"
 
 const int kButtonCount = 3;
 const int kButtonPins[] = {2, 3, 4};
 const int kLedPins[] = {17, 16, 15};
 const int kResetButtonPin = 6;
+const int kSpeakerPin = 8;
 
 bool gotWinner = false;
 
@@ -11,10 +13,11 @@ void setup() {
   for (const int buttonPin : kButtonPins) {
     pinMode(buttonPin, INPUT);
   }
-  pinMode(kResetButtonPin, INPUT);
   for (const int ledPin : kLedPins) {
     pinMode(ledPin, OUTPUT);
   }
+  pinMode(kResetButtonPin, INPUT);
+  pinMode(kSpeakerPin, OUTPUT);
 }
 
 void loop() {
@@ -24,6 +27,9 @@ void loop() {
       gotWinner = false;
       for (int ledPin : kLedPins) {
         digitalWrite(ledPin, LOW);
+        tone(kSpeakerPin, NOTE_D4);
+        delay(100/*ms*/);
+        noTone(kSpeakerPin);
       }
     }
   } else {
@@ -31,6 +37,9 @@ void loop() {
       int buttonState = digitalRead(kButtonPins[i]);
       if (buttonState == HIGH) {
         digitalWrite(kLedPins[i], HIGH);
+        tone(kSpeakerPin, NOTE_G4);
+        delay(100/*ms*/);
+        noTone(kSpeakerPin);
         gotWinner = true;
         break;
       }
