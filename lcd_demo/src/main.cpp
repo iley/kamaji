d2d1a249159d2661a8e0987a306049b5a6a90b25
Ilya Strukov <iley@iley.ru>
2017-05-20 15:11:21 +0200

    make the i2c lcd work

--- lcd_demo/src/main.cpp
+++ lcd_demo/src/main.cpp
@@ -1,13 +1,30 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x38);
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+
+LiquidCrystal_I2C lcd(0x3f,2,1,0,4,5,6,7);
+
+const char kProgressChars[] = {'/', '-', '\\', '|'};
 
 void setup() {
   lcd.begin(/*cols=*/16, /*rows=*/2);
+
+  lcd.setBacklightPin(3, POSITIVE);
+  lcd.setBacklight(HIGH);
+
   lcd.home();
   lcd.print("Hello world!");
+  delay(1000/*ms*/);
 }
 
 void loop() {
+  int i = 0;
+  while (true) {
+    lcd.setCursor(0, 1);
+    lcd.print(kProgressChars[i]);
+    i = (i + 1) % ARRAY_SIZE(kProgressChars);
+    delay(1000/*ms*/);
+  }
 }
