e1e004b895048ab396b30365cd96e4bc127d85f5
Ilya Strukov <iley@iley.ru>
2017-05-20 15:25:26 +0200

    change the animation

--- lcd_demo/src/main.cpp
+++ lcd_demo/src/main.cpp
@@ -6,25 +6,20 @@
 
 LiquidCrystal_I2C lcd(0x3f,2,1,0,4,5,6,7);
 
-const char kProgressChars[] = {'/', '-', '\\', '|'};
-
 void setup() {
   lcd.begin(/*cols=*/16, /*rows=*/2);
 
   lcd.setBacklightPin(3, POSITIVE);
   lcd.setBacklight(HIGH);
-
-  lcd.home();
-  lcd.print("Hello world!");
-  delay(1000/*ms*/);
 }
 
 void loop() {
-  int i = 0;
-  while (true) {
-    lcd.setCursor(0, 1);
-    lcd.print(kProgressChars[i]);
-    i = (i + 1) % ARRAY_SIZE(kProgressChars);
-    delay(1000/*ms*/);
+  lcd.clear();
+  lcd.home();
+  lcd.print("Hello world!");
+  lcd.setCursor(0, 1);
+  for (int i = 0; i < 16; ++i) {
+    lcd.print('.');
+    delay(500/*ms*/);
   }
 }
