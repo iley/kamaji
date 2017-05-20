b8765f37e537beb7ac7a4b3d815a8e74d4f98592
Ilya Strukov <iley@iley.ru>
2017-05-20 15:26:31 +0200

    cleanup

--- lcd_demo/src/main.cpp
+++ lcd_demo/src/main.cpp
@@ -2,13 +2,10 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
-
 LiquidCrystal_I2C lcd(0x3f,2,1,0,4,5,6,7);
 
 void setup() {
   lcd.begin(/*cols=*/16, /*rows=*/2);
-
   lcd.setBacklightPin(3, POSITIVE);
   lcd.setBacklight(HIGH);
 }
