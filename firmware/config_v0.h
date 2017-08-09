8cdf04c545199f789d9fbdd8a6b6ac3db45cb11b
Ilya Strukov <iley@iley.ru>
2017-08-09 22:47:45 +0200

    create version-specific header files for configuration

---
+++ firmware/config_v0.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_V0_H
+#define CONFIG_V0_H
+
+#define USE_I2C_LCD 1
+
+// TODO: Define pin numbers here.
+
+#endif  // CONFIG_V0_H
---
+++ firmware/config_v1.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_V1_H
+#define CONFIG_V1_H
+
+// TODO: Define pin numbers here.
+
+#endif  // CONFIG_V1_H
