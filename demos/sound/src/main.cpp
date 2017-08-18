8c53b2038e1c8d9ee3563847d48cb44d6c16a97b
Ilya Strukov <iley@iley.ru>
2017-08-18 11:44:27 +0200

    update the sound demo for v1

--- demos/sound/src/main.cpp
+++ demos/sound/src/main.cpp
@@ -1,20 +1,14 @@
 #include "Arduino.h"
 #include "pitches.h"  // Taken from https://www.arduino.cc/en/Tutorial/ToneMelody
 
-const int kSpeakerPin = 11;
-const int kMelody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-};
+const int kSpeakerPin = 9;
+const int freq = 1200;
 
 void setup() {
   pinMode(kSpeakerPin, OUTPUT);
 }
 
 void loop() {
-  for (auto freq : kMelody) {
-    tone(kSpeakerPin, freq);
-    delay(200);
-    noTone(kSpeakerPin);
-  }
+  tone(kSpeakerPin, freq, 501);
   delay(1000);
 }
