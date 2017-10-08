cadbb62383e14cf650dcf1a64aba774e4bcc2c51
Ilya Strukov <iley@iley.ru>
2017-10-08 09:55:38 +0200

    add teensy to demo/sound

--- demos/sound/src/main.cpp
+++ demos/sound/src/main.cpp
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 #include "pitches.h"  // Taken from https://www.arduino.cc/en/Tutorial/ToneMelody
 
-const int kSpeakerPin = 9;
+const int kSpeakerPin = 2;
 const int freqs[] = {100, 500, 1000, 1500, 2000, 2500, 3000};
 
 void setup() {
