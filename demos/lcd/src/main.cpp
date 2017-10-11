d147a5b84d2a5a815f68dba336845955063ea229
Ilya Strukov <iley@iley.ru>
2017-10-11 15:43:03 +0200

    russian lcd

--- demos/lcd/src/main.cpp
+++ demos/lcd/src/main.cpp
@@ -1,47 +1,57 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+/*
+  LiquidCrystal Library - Hello World
+ 
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the 
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+ 
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+ 
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD RW pin to digital pin 10
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ 
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 25 July 2009
+ by David A. Mellis
+ modified 25 May 2010
+ by Ilya V. Danilov http://mk90.ru/
 
-#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+ */
 
-LiquidCrystal_I2C lcd(0x3f,2,1,0,4,5,6,7);
+// include the library code:
+#include "Arduino.h"
+#include <LiquidCrystalRus.h>
 
-const uint8_t kCharBitmap[][8] = {
-   { 0xc, 0x12, 0x12, 0xc, 0, 0, 0, 0 },
-   { 0x6, 0x9, 0x9, 0x6, 0, 0, 0, 0 },
-   { 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0, 0x0 },
-   { 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0, 0x0 },
-   { 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0x0 },
-   { 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0x0 },
-   { 0x0, 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0x0 },
-   { 0x0, 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0x0 },
-};
+// initialize the library with the numbers of the interface pins
+LiquidCrystalRus lcd(16, 15, 14, 13, 12, 11, 10);
 
 void setup() {
-  lcd.begin(/*cols=*/16, /*rows=*/2);
-  for (unsigned int i = 0; i < ARRAY_SIZE(kCharBitmap); ++i) {
-    lcd.createChar(i, (uint8_t*)kCharBitmap[i]);
-  }
-  lcd.setBacklightPin(3, POSITIVE);
-  lcd.setBacklight(HIGH);
+  // set up the LCD's number of rows and columns: 
+  lcd.begin(20, 4);
+  // Print a message to the LCD.
+  lcd.print("Привет, мир!");
 }
 
 void loop() {
-  lcd.clear();
-  lcd.home();
-  lcd.print("Hello world!");
-
-  for (int i = 0; i < 256; i += 2) {
-    lcd.setCursor(/*col=*/0, /*row=*/1);
-    lcd.print(i);
-    lcd.setCursor(/*col=*/4, /*row=*/1);
-    lcd.print((char)i);
-    if (i + 1 < 256) {
-      lcd.setCursor(/*col=*/8, /*row=*/1);
-      lcd.print(i + 1);
-      lcd.setCursor(/*col=*/12, /*row=*/1);
-      lcd.print((char)(i + 1));
-    }
-    delay(500);
-  }
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis()/1000);
 }
