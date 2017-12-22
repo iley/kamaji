567d99393e99b1c2d5c6d47e886e964e9af60b57
Ilya Strukov <iley@iley.ru>
2017-12-22 19:29:25 +0100

    draw a bitmap in the 128x64 LCD demo

--- demos/lcd12864/src/main.cpp
+++ demos/lcd12864/src/main.cpp
@@ -5,17 +5,20 @@
 
 #include "bitmap.h"
 
-U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, 41/*CS*/, 40/*RESET*/);
+U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, A5/*CS, pin 41*/, A4/*RESET, pin 40*/);
 
 const uint8_t bitmap[] PROGMEM = { BITMAP_DATA };
 
 void setup(void) {
   u8g2.begin();
   u8g2.enableUTF8Print();
+  u8g2.setDrawColor(1);
+  u8g2.setBitmapMode(1);
 }
 
 void loop(void) {
   u8g2.setFont(u8g2_font_haxrcorp4089_t_cyrillic);
+
   u8g2.firstPage();
   do {
     u8g2.setCursor(0, 8);
@@ -24,6 +27,14 @@ void loop(void) {
     u8g2.print(F("французских булок,"));
     u8g2.setCursor(0, 24);
     u8g2.print(F("да выпей же чаю."));
-  } while ( u8g2.nextPage() );
-  delay(1000);
+  } while (u8g2.nextPage());
+  delay(5000);
+
+  u8g2.clearDisplay();
+
+  u8g2.firstPage();
+  do {
+    u8g2.drawXBMP(0, 0, BITMAP_WIDTH, BITMAP_HEIGHT, bitmap);
+  } while (u8g2.nextPage());
+  delay(5000);
 }
