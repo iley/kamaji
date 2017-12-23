dae35f7f811813ab27a2c50ecf10d38c21caa748
Ilya Strukov <iley@iley.ru>
2017-12-23 12:38:03 +0100

    display pressed buttons, play tone and flash LEDs

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 
 #include "lcd.h"
+#include "xpins.h"
+
+const uint32_t kDebounceMs = 100;
+const uint32_t kFlashMs = 1000;
+const uint16_t kToneHz = 1000;
+const uint32_t kToneMs = 300;
 
 DECLARE_KBUTTON_PINS();
 DECLARE_KLED_PINS();
@@ -13,10 +19,61 @@ DECLARE_KLAMP_PIN();
 
 bool buttons[BUTTON_COUNT] = { false };
 bool buttonsBefore[BUTTON_COUNT] = { false };
-unsigned long lastPressedMs[BUTTON_COUNT] = {0};
+uint32_t lastPressedMs[BUTTON_COUNT] = { 0 };
+uint32_t flashStartMs[PLAYER_COUNT] = { 0 };
 
 void setup() {
+  for (const int buttonPin : kButtonPins) {
+    xPinMode(buttonPin, INPUT_PULLUP);
+  }
+  for (const int ledPin : kLedPins) {
+    xPinMode(ledPin, OUTPUT);
+    xDigitalWrite(ledPin, LOW);
+  }
+  xPinMode(kSpeakerPin, OUTPUT);
+
+#if USE_LAMP
+  xPinMode(kLampPin, OUTPUT);
+#endif
+
+  lcd.begin(DISPLAY_COLS, DISPLAY_ROWS);
+  ENABLE_LCD_BACKLIGHT();
+
+  tone(kSpeakerPin, kToneHz, kToneMs);
 }
 
 void loop() {
+  uint32_t now = millis();
+  for (int i = 0; i < BUTTON_COUNT; ++i) {
+    if (i == 0) continue; // FIXME: skipping the first button since it's stuck on v0.
+    buttonsBefore[i] = buttons[i];
+    if (now - lastPressedMs[i] > kDebounceMs) {
+      if (xDigitalRead(kButtonPins[i]) == LOW) {
+        buttons[i] = true;
+        lastPressedMs[i] = now;
+      } else {
+        buttons[i] = false;
+      }
+    }
+
+    if (buttons[i] && !buttonsBefore[i]) {
+      lcd.clear();
+      lcd.print("Pressed ");
+      lcd.print(i);
+      tone(kSpeakerPin, kToneHz, kToneMs);
+
+      if (FIRST_PLAYER_BUTTON <= i && i <= LAST_PLAYER_BUTTON) {
+        int playerNumber = i - FIRST_PLAYER_BUTTON;
+        int ledPin = kLedPins[playerNumber];
+        digitalWrite(ledPin, HIGH);
+        flashStartMs[playerNumber] = now;
+      }
+    }
+  }
+
+  for (int i = 0; i < PLAYER_COUNT; ++i) {
+    if (flashStartMs[i] != 0 && now - flashStartMs[i] > kFlashMs) {
+      digitalWrite(kLedPins[i], LOW);
+    }
+  }
 }
