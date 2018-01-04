f480e49c37e6329675e11cf027eb54b90cdfbea2
Ilya Strukov <iley@iley.ru>
2018-01-04 22:04:33 +0100

    try using software emulated SPI for the LCD

--- firmware/src/graphic_lcd.h
+++ firmware/src/graphic_lcd.h
@@ -7,10 +7,16 @@
 #include <Print.h>
 #include <U8g2lib.h>
 
+#define GRAPHIC_LCD_USE_SOFTWARE_SPI 1
+
 class GraphicLcd : public Print {
   public:
     GraphicLcd(uint8_t chip_select_pin, uint8_t reset_pin)
+#if GRAPHIC_LCD_USE_SOFTWARE_SPI
+      : u8g2_(U8G2_R0, 15, 16, chip_select_pin, reset_pin) {}
+#else
       : u8g2_(U8G2_R0, chip_select_pin, reset_pin) {}
+#endif
     void begin(uint8_t cols, uint8_t rows);
     void clear();
     void setCursor(uint8_t col, uint8_t row);
@@ -27,7 +33,11 @@ class GraphicLcd : public Print {
 
     void clearBuffer();
 
+#if GRAPHIC_LCD_USE_SOFTWARE_SPI
+    U8G2_ST7920_128X64_1_SW_SPI u8g2_;
+#else
     U8G2_ST7920_128X64_1_HW_SPI u8g2_;
+#endif
     char buffer_[MAX_ROWS][MAX_COLS+1];
     uint8_t row_ = 0;
     uint8_t col_ = 0;
