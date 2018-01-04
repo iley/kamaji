19959d3ad5f9307fd2c0fb8a0a26206814c65ed7
Ilya Strukov <iley@iley.ru>
2018-01-04 21:46:47 +0100

    use LCD in the paged mode to save memory

--- firmware/src/graphic_lcd.cpp
+++ firmware/src/graphic_lcd.cpp
@@ -2,24 +2,46 @@
 #include "graphic_lcd.h"
 
 void GraphicLcd::begin(uint8_t cols, uint8_t rows) {
-  // Ignore cols and rows for now.
-  u8g2.begin();
-  u8g2.enableUTF8Print();
-  u8g2.setFont(u8g2_font_haxrcorp4089_t_cyrillic);
+  cols_ = cols < MAX_COLS ? cols : MAX_COLS;
+  rows_ = rows < MAX_ROWS ? rows : MAX_ROWS;
+  u8g2_.begin();
   clear();
 }
 
 void GraphicLcd::clear() {
-  u8g2.clearBuffer();
   setCursor(0, 0);
+  clearBuffer();
+  flush();
 }
 
 void GraphicLcd::setCursor(uint8_t col, uint8_t row) {
-  u8g2.setCursor(kGraphicLcdCharWidth * row, kGraphicLcdCharHeight * (col+1));
+  col_ = col % cols_;
+  row_ = row % rows_;
 }
 
 void GraphicLcd::flush() {
-  u8g2.sendBuffer();
+  u8g2_.firstPage();
+  do {
+    u8g2_.setFont(u8g2_font_haxrcorp4089_t_cyrillic);
+    for (int i = 0; i < rows_; ++i) {
+      u8g2_.drawStr(0, (i+1) * CHAR_HEIGHT, buffer_[i]);
+    }
+  } while (u8g2_.nextPage());
+}
+
+size_t GraphicLcd::write(uint8_t ch) {
+  buffer_[row_][col_] = ch;
+  col_ = (col_ + 1) % cols_;
+  return 1;
+}
+
+void GraphicLcd::clearBuffer() {
+  for (int i = 0; i < MAX_ROWS; ++i) {
+    for (int j = 0; j < MAX_COLS; ++j) {
+      buffer_[i][j] = ' ';
+    }
+    buffer_[i][MAX_COLS] = 0;
+  }
 }
 
 
--- firmware/src/graphic_lcd.h
+++ firmware/src/graphic_lcd.h
@@ -4,23 +4,35 @@
 #if USE_GRAPHIC_LCD
 
 #include <SPI.h>
+#include <Print.h>
 #include <U8g2lib.h>
 
-constexpr uint8_t kGraphicLcdCharWidth = 8;
-constexpr uint8_t kGraphicLcdCharHeight = 8;
-
-class GraphicLcd {
+class GraphicLcd : public Print {
   public:
     GraphicLcd(uint8_t chip_select_pin, uint8_t reset_pin)
-      : u8g2(U8G2_R0, chip_select_pin, reset_pin) {}
+      : u8g2_(U8G2_R0, chip_select_pin, reset_pin) {}
     void begin(uint8_t cols, uint8_t rows);
     void clear();
     void setCursor(uint8_t col, uint8_t row);
     void flush();
-    template<typename T> void print(T value) { u8g2.print(value); }
+    size_t write(uint8_t ch) override;
 
-    U8G2_ST7920_128X64_F_HW_SPI u8g2;
   private:
+    enum {
+      MAX_COLS = 16,
+      MAX_ROWS = 8,
+      CHAR_WIDTH = 8,
+      CHAR_HEIGHT = 8,
+    };
+
+    void clearBuffer();
+
+    U8G2_ST7920_128X64_1_HW_SPI u8g2_;
+    char buffer_[MAX_ROWS][MAX_COLS+1];
+    uint8_t row_ = 0;
+    uint8_t col_ = 0;
+    uint8_t rows_ = MAX_ROWS;
+    uint8_t cols_ = MAX_COLS;
 };
 
 #endif  // USE_GRAPHIC_LCD
